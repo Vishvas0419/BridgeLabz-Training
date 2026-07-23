cbbebfbe3dfee06832453deface7a07e7679d39b
Vishvas Vaglay <vishvas0419@gmail.com>
2026-07-23 11:44:40 +0530

    Vishvas0419 add, Initial commit with core programming basics and operators code

---
+++ dotnet-core-programming-basics/01-basics/ReadWriteBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReadWriteBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You entered: {i}");
+
+            Console.Write("Enter a double: ");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine($"You entered: {d}");
+
+            Console.Write("Enter a string: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"You entered: {s}");
+
+            Console.WriteLine("Enter a boolean: ");
+            bool b = bool.Parse(Console.ReadLine());
+            Console.WriteLine($"You entered: {b}");
+
+            Console.WriteLine("Enter a character: ");
+            char c = char.Parse(Console.ReadLine());
+            Console.WriteLine($"You entered: {c}")
+        }
+    }
+}
---
+++ dotnet-core-programming-basics/01-basics/TypeConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numStr = "123";
+            int num = int.Parse(numStr);
+            Console.WriteLine("String to int: " + num);
+
+            double dNum = num;
+            Console.WriteLine("Implicit int to double: " + dNum);
+
+            double dValue = 45.67;
+            int iValue = (int)dValue;
+            Console.WriteLine("Explicit double to int: " + iValue);
+
+            string backToString = iValue.ToString();
+            Console.WriteLine("Int to string: " + backToString);
+        }
+    }
+}
