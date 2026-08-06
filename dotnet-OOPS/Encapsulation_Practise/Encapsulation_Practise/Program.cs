f877d7a5f23686bb4ea37c000695f58be3504f5d
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-06 22:22:33 +0530

    practised access modifiers

---
+++ dotnet-OOPS/Encapsulation_Practise/Encapsulation_Practise/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Encapsulation_Practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World to Encapuslation Practise!");
+
+
+            Student s1 = new Student(23); //now contructor is assigning value 23 to the age instance variable during Object creation
+
+            //now current value of age = 23
+
+            Console.WriteLine(s1.Age); //23 //using getter property
+
+            s1.Age = 18; //using setter property //value = 18 here
+
+            //Console.WriteLine(s1.age); //if age instance variable/field is set to be public then we dont need to use getter or setter property and the data hiding property is destroyed we can simply use that variable oinsteda of using Age property
+
+            s1.SetAge(40); //setting age using SetAge() method this is equal to s1.Age = 40
+
+            Console.WriteLine(s1.GetAge()); //accessing age using custom GetAge() method
+
+
+
+            
+
+            
+        }
+    }
+}
---
+++ dotnet-OOPS/Encapsulation_Practise/Encapsulation_Practise/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation_Practise
+{
+    internal class Student
+    {
+        private int age; //field
+
+        public Student() { }
+        public Student(int age)
+        {
+            this.age = age;
+        }
+        public int Age //property
+        {
+            get { return age; }
+
+            set { age = value; }
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+    }
+    internal class Student2
+    {
+        private int age { get; set; } //property but age is field
+
+    }
+
+    internal class Student3
+    {
+        private int age;
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+    }
+}
