da11d9cf6f9d2501e7c5ecab5a80dbbf90b5c76a
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-10 22:29:10 +0530

    Practise OOP in C#

---
+++ dotnet-Collections-Streams/Collections/Collections/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            List<int> reversedCopy = ListReverser.Reverse(numbers);
+            Console.WriteLine(string.Join(", ", reversedCopy));
+
+            ListReverser.ReverseInPlace(numbers);
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ dotnet-Collections-Streams/Collections/Collections/ReverseList.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    public class ListReverser
+    {
+        public static List<int> Reverse(List<int> input)
+        {
+            List<int> result = new List<int>();
+            int index = input.Count - 1;
+            while (index >= 0)
+            {
+                result.Add(input[index]);
+                index--;
+            }
+            return result;
+        }
+
+    }}
