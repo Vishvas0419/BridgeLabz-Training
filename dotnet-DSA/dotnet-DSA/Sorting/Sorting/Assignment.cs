d46252d2b5d63357e4c8d3fdf988f73b157bc4d3
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-14 12:47:08 +0530

    Assignment questions of sorting

---
+++ dotnet-DSA/dotnet-DSA/Sorting/Sorting/Assignment.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Sorting
+{
+    internal static class Assignment
+    {
+        public static void BubbleSort(int[]arr) //adj elements are swapped
+        {
+            int n = arr.Length;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n-1;j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(int[]arr) //Take one element at a time and insert it into its correct position in the already-sorted portion.
+        {
+            int n = arr.Length;
+            for(int i=1;i<n;i++)
+            {
+                int j = i;
+                while(j>0 && arr[j] < arr[j-1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    j--;
+                }
+            }
+        }
+
+        public static void SelectionSort(int[]arr)
+        {
+            int n=arr.Length;
+            for(int i=0;i<n;i++)
+            {
+                int minIndex = i;
+                for(int j=i+1;j<n;j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        public static void MergeSort(int[] arr, int low, int high)
+        {
+            // Base case
+            if (low >= high)
+                return;
+
+            int mid = low + (high - low) / 2;
+
+            MergeSort(arr, low, mid);
+            MergeSort(arr, mid + 1, high);
+
+            Merge(arr, low, mid, high);
+        }
+
+        private static void Merge(int[] arr, int low, int mid, int high)
+        {
+            int[] temp = new int[high - low + 1];
+
+            int left = low;
+            int right = mid + 1;
+            int i = 0;
+
+            while (left <= mid && right <= high)
+            {
+                if (arr[left] <= arr[right])
+                {
+                    temp[i++] = arr[left++];
+                }
+                else
+                {
+                    temp[i++] = arr[right++];
+                }
+            }
+
+            while (left <= mid)
+            {
+                temp[i++] = arr[left++];
+            }
+
+            while (right <= high)
+            {
+                temp[i++] = arr[right++];
+            }
+
+            // Copy back into original array
+            for (int j = 0; j < temp.Length; j++)
+            {
+                arr[low + j] = temp[j];
+            }
+        }
+
+
+        public static void QuickSort(int[] prices, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(prices, low, high);
+
+                QuickSort(prices, low, pivotIndex - 1);
+
+                QuickSort(prices, pivotIndex + 1, high);
+            }
+        }
+
+        public static int Partition(int[] prices, int low, int high)
+        {
+            int pivot = prices[high];
+
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (prices[j] < pivot)
+                {
+                    i++;
+
+                    int temp = prices[i];
+                    prices[i] = prices[j];
+                    prices[j] = temp;
+                }
+            }
+
+            int temp2 = prices[i + 1];
+            prices[i + 1] = prices[high];
+            prices[high] = temp2;
+
+            return i + 1;
+        }
+
+    }
+}
---
+++ dotnet-DSA/dotnet-DSA/Sorting/Sorting/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] BubbleArray = {2,3,4,1,6,7,1,3,4,65,8};
+            Assignment.BubbleSort(BubbleArray);
+            PrintArray(BubbleArray);
+
+            int[] insertionArray = { 2, 3, 4, 1, 5, 6, 7, 1, 3 };
+            Assignment.InsertionSort(insertionArray);
+            PrintArray(insertionArray);
+
+            int[]selectionArray = { 2, 3, 4,1, 5, 6,7, 1, 3 };
+            Assignment.SelectionSort(selectionArray);
+            PrintArray(selectionArray);
+
+            int[] mergeArray = { 2, 3, 4, 5, 8, 10 };
+            Assignment.MergeSort(mergeArray, 0, mergeArray.Length);
+            PrintArray(mergeArray);
+
+            int[] prices = { 500, 200, 800, 100, 400 };
+            Assignment.QuickSort(prices, 0, prices.Length - 1);
+            PrintArray(prices);
+
+        }
+        public static void PrintArray(int[]arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
