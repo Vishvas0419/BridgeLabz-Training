7f8bd680038a670e774d8f5d36694255720c2b39
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-14 09:54:54 +0530

    Assignment questions of stack

---
+++ dotnet-DSA/dotnet-DSA/Stacks-Queues/Stacks-Queues/Assignment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks_Queues
+{
+    internal class Assignment
+    {
+        public void SortStack(Stack<int> stack)
+        {
+            if (stack.Count > 0)
+            {
+                int top = stack.Pop();
+                SortStack(stack);
+                InsertSorted(stack, top);
+            }
+        }
+
+        private void InsertSorted(Stack<int> stack, int value)
+        {
+            if (stack.Count == 0 || stack.Peek() >= value)
+            {
+                stack.Push(value);
+            }
+            else
+            {
+                int top = stack.Pop();
+                InsertSorted(stack, value);
+                stack.Push(top);
+            }
+        }
+    }
+}
---
+++ dotnet-DSA/dotnet-DSA/Stacks-Queues/Stacks-Queues/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Stacks_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World this main class for stacks and queues!");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(4);
+            stack.Push(3);
+            stack.Push(6);
+            stack.Push(7);
+            stack.Push(1);
+            stack.Push(5);
+            stack.Push(2);
+            Console.WriteLine("Before sorting, original stack : ");
+            PrintStack(stack);
+            Assignment ass = new Assignment();
+            ass.SortStack(stack);
+            Console.WriteLine("Sorted Stack : ");
+            PrintStack(stack);
+        }
+
+        private static void PrintStack(Stack<int>stack)
+        {
+            foreach (var item in stack)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
