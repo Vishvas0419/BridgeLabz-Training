f2903f784c7a3c23bc19c9c3d32b00ef57baf17d
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-18 23:51:36 +0530

    Searching in c#

---
+++ dotnet-DSA/dotnet-DSA/Searching/Searching/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\BridgeLabz-Training\\dotnet-DSA\\dotnet-DSA\\Searching\\Searching\\source.txt";
+            string targetword = "java";
+            string text = File.ReadAllText(filePath);
+            string[] words = text.Split(' ');
+            int index = 0;
+            for(int i=0;i<words.Length;i++) //linear search
+            {
+                if(words[i].ToLower() == targetword.ToLower())
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index != -1)
+            {
+                Console.WriteLine($"Word {targetword} found at index : {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Word {targetword} not found in the source file");
+            }
+
+
+        }
+    }
+}
