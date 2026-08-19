2efd60b1b0175b1a9601744da0fb64273a389811
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-19 18:54:43 +0530

    week3 review code

--- Reviews/Reviews/Program.cs
+++ Reviews/Reviews/Program.cs
@@ -100,7 +100,9 @@ namespace Reviews
             //Console.WriteLine("Result : " + SmallestGap(arr,n));
 
             //project 2 moview streaming platform
+
             RegexParse parser = new RegexParse();
+
             ProfileStore<ViewingSession> store = new ProfileStore<ViewingSession>();
 
             string record = "USER:Alice|TITLE:Mission: Impossible - Fallout (2018)|GENRES:sci-fi,drama,thriller|WATCHED:80%|TS:2026-08-14T21:10:00";
@@ -108,6 +110,7 @@ namespace Reviews
             ViewingSession session = parser.Parse(record);
             session.display();
 
+            //store is object to use the ProfileStore generic class methods
             store.AddSession(session);
 
             Dictionary<string, double> profile = store.GetProfile("Alice");
@@ -115,6 +118,36 @@ namespace Reviews
             {
                 Console.WriteLine(pair.Key + " : " + pair.Value);
             }
+
+            List<ViewingSession> highWatchedSessions = store.GetHighlyWatchedSessions(80);
+            Console.WriteLine("highly watched sessions : ");
+
+            foreach(ViewingSession s in  highWatchedSessions)
+            {
+                Console.WriteLine(s.Title + " : " + s.WatchedPercentage + "%");
+            }
+
+
+            //==============================================================
+            //Pyramid Arrangement
+            int[] input = { 1, 4, 3, 6, 8, 7, 9, 2, 5, 0, 12, 23, -1 };
+            int n = input.Length;
+            int[] output = Week3.pyramid(input, n);
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.Write(output[i] + " ");
+            }
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
--- Reviews/Reviews/Week3.cs
+++ Reviews/Reviews/Week3.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Text.RegularExpressions;
-//@"USER:(?<Username>[^|]+)\|TITLE:(?<Title>.+?)\s*\((?<Year>\d{4})\)\|GENRES:(?<Genres>[^|]+)\|WATCHED:(?<Watched>\d+)%\|TS:(?<Timestamp>[^|]+)"
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Reviews
 {
-    internal class Week3
+    internal static class Week3
     {
+        public static int[] pyramid(int[] arr, int n)
+        {
+            Array.Sort(arr);
+            int[] result = new int[n];
+            int middle = n / 2;
+            int left = middle - 1;
+            int right = middle + 1;
+            result[middle] = arr[n - 1];
 
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if ((n - 2 - i) % 2 != 0)
+                    result[left--] = arr[i];
+                else
+                    result[right++] = arr[i];
+            }
+            return result;
+        }
     }
-
     public class ViewingSession
     {
         private string username;
@@ -60,8 +84,6 @@ namespace Reviews
 
     }
 
-
-
     public class ProfileStore<T> where T : ViewingSession
     {
         private Dictionary<string, Dictionary<string, double>> userProfiles;
@@ -70,9 +92,10 @@ namespace Reviews
         public ProfileStore()
         {
             userProfiles = new Dictionary<string, Dictionary<string, double>>();
-            sessions = new List<T>();
+            sessions = new List<T>();   
         }
 
+        //store session in a list and then update the userProfile (weight)
         public void AddSession(T session)
         {
             sessions.Add(session);
@@ -106,9 +129,16 @@ namespace Reviews
         {
             return sessions;
         }
+        //LINQ method to filter highly watched sessions
+        public List<T> GetHighlyWatchedSessions(double percentage)
+        {
+            return sessions.Where(session => session.WatchedPercentage >= percentage)
+                .ToList();
+        }
     }
 
-    public class RegexParse
+    //main kaam of this regex Parse class Parse() method is to convert raw string to a object and store it in a object of Viewing session (session object here) and return it
+    public class RegexParse //contains Parse() method
     {
         private string pattern = @"USER:(.*?)\|TITLE:(.*?)\s*\((\d{4})\)\|GENRES:(.*?)\|WATCHED:(\d+)%\|TS:(.*)";
         public ViewingSession Parse(string input)
@@ -137,6 +167,30 @@ namespace Reviews
             return session;
         }
     }
-}
+
+
+
+    //============================================================
+
+
+    //Week 3 coding question - Pyramid Arrangement
+
+//Given to you is an unordered / unsorted integer array of size n.Reorder the array in such a way that the largest number of the array is in the middle index of the array and the remaining numbers which are lesser than or equal to the previous element should be arranged in descending order as we move to the edges of the array ((0, n-1) index).
+//Note that as we move to the edges of the array, you will need to arrange the elements in the descending order starting from the middle index.You have to place the next largest after the largest element (the middle element) on either side of the middle element.
+//If n is even, you can consider either of the n / 2 - 1'^ or n/2t^ index as your middle element and place the largest element in either of those indexes and move towards edges from there.
+//Example:
+//Input:
+//1, 4, 3, 6, 8, 7, 9, 2, 5, 0, 12, 23, -1
+//Output:
+//-1, 1, 3, 5, 7, 9, 23, 12, 8, 6, 4, 2, 0
+//int[] pyramid(int arr[], int n) {
+//……………..your code
+//……………
+//return arr[ ]
+//}
+
+    
+
+    }
 
 
