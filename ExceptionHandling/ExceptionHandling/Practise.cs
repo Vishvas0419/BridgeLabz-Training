66c33bd5d676eedc467fa5c4a06b6b6dffb0f34c
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-20 23:52:52 +0530

    Exception handling in c#

---
+++ ExceptionHandling/ExceptionHandling/Practise.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    internal static class Practise
+    {
+        public static void DivideByZero()
+        {
+            try
+            {
+                Console.WriteLine("A");
+
+                int a = 10; int b = 0;
+                int x = a/b;
+
+                Console.WriteLine("B"); //C# immediately stops executing the remaining statements inside the try when Exception occurs
+            }
+            catch (DivideByZeroException e) //e is a Object of class Exception
+            {
+                Console.WriteLine("C");
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine(e.StackTrace); //tells where exception occurs
+                //Console.WriteLine(e.ToString);
+
+            }
+            catch(IndexOutOfRangeException) //without obj if you dont want any info about the catched exception 
+            {
+                Console.WriteLine("Index out of bound please check your indices");
+            }
+
+            Console.WriteLine("D"); //after handling the exception the program flow continue after try catch block
+        }
+
+
+        //exception propagation in nested method
+        static void MethodA()
+        {
+            MethodB();
+        }
+        static void MethodB()
+        {
+            MethodC();
+        }
+        static void MethodC()
+        {
+            int a = 10; int b = 0;
+            int x = a / b;
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Practise.DivideByZero();
+        }
+    }
+}
