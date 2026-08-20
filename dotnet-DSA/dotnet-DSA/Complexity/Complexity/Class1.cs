8481801ececbe7a0ee2a7826e5139381c1c409a5
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-20 11:48:36 +0530

    Complexity

---
+++ dotnet-DSA/dotnet-DSA/Complexity/Complexity/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+namespace Complexity
+{
+    internal static class Class1
+    {
+
+        public static int LinearSeearch(int[]arr,int target)
+        {
+            int n = arr.Length;
+            for(int i=0;i<n;i++)
+            {
+                if (arr[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        //1 2 3 3 4 5 6 6 6 7 7 7 8 8  9 9 
+        public static int BinarySearch(int[]arr,int target)
+        {
+            int n = arr.Length;
+            int low = 0;
+            int high = n - 1;
+            while(low < high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (arr[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else low = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ dotnet-DSA/dotnet-DSA/Complexity/Complexity/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Complexity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw1 = Stopwatch.StartNew();
+            Random random = new Random();
+
+            int n = 1000;
+            int[] arr = new int[n];
+            int target = arr[n - 1];
+            //int n = 1000000;
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = random.Next(1, n * 10);
+            }
+            Array.Sort(arr);
+            sw1.Start();
+            int LinearResult = Class1.LinearSeearch(arr, target);
+            sw1.Stop();
+
+            Stopwatch sw2 = Stopwatch.StartNew();
+            sw2.Start();
+            int BinaryResult = Class1.BinarySearch(arr, target);
+            sw2.Stop();
+            Console.WriteLine("Result of linear serach : "+ LinearResult + " Time Taken in linear Search : "+sw1.Elapsed.TotalMilliseconds);
+            Console.WriteLine("Result of Binary Search : "+BinaryResult + " Time Taken in Binary Search : "+sw2.Elapsed.TotalMilliseconds);
+        }
+    }
+}
