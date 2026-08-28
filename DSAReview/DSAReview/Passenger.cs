e8a481614c3898c4aaa8ee1e8347321ae97d27f0
Vishvas Vaglay <vishvas0419@gmail.com>
2026-08-28 19:35:48 +0530

    DSA Week4 Review

--- DSAReview/DSAReview/Passenger.cs
+++ DSAReview/DSAReview/Passenger.cs
@@ -5,7 +5,6 @@
         public int PassengerId { get; set; }
         public string Name { get; set; }
         public bool IsPriority { get; set; }
-
         public Passenger(int passengerId, string name, bool isPriority)
         {
             PassengerId = passengerId;
