76bfedb3aa6bda738ccea661220ed9e4df6f9c68
Vishvas Vaglay <vishvas0419@gmail.com>
2026-09-09 12:56:01 +0530

    Added test file for week 6

---
+++ Reviews/TestProjectReview/Week6Tests.cs
@@ -0,0 +1,37 @@
+﻿using Reviews;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestProjectReview
+{
+    internal class Week6Tests
+    {
+        Week6 week6 = new Week6();
+        [Test]
+        public void CorrectSubtotal()
+        {
+            Product product = new Product
+            {
+                BillId = "BILL1",
+                Item = "Notebook",
+                Qty = 5,
+                UnitPrice = 40,
+                Category = "STATIONERY"
+            };
+            decimal result = week6.CalculateSubTotal(product);
+            Assert.That(result, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void CorrectTaxCalculation()
+        {
+            //Product product = new Product
+            //{
+            //    BillId = 
+            //}
+        }
+
+    }
+}
