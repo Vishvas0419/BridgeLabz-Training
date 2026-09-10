e1ae3b9aba5114448642ef18b0ffddd75f0a1734
Vishvas Vaglay <vishvas0419@gmail.com>
2026-09-10 12:24:01 +0530

    added week 6 test file

--- Reviews/TestProjectReview/Week6Tests.cs
+++ Reviews/TestProjectReview/Week6Tests.cs
@@ -1,18 +1,21 @@
-﻿using Reviews;
-using System;
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Reviews;
 using System.Collections.Generic;
-using System.Text;
-using NUnit.Framework;
 
 namespace TestProjectReview
 {
     internal class Week6Tests
     {
-        Week6 week6 = new Week6();
-        [Test]
-        public void CorrectSubtotal()
+        Week6 week6;
+        Product product;
+
+        [SetUp]
+        public void Setup()
         {
-            Product product = new Product
+            week6 = new Week6();
+
+            product = new Product
             {
                 BillId = "BILL1",
                 Item = "Notebook",
@@ -20,18 +23,115 @@ namespace TestProjectReview
                 UnitPrice = 40,
                 Category = "STATIONERY"
             };
-            decimal result = week6.CalculateSubTotal(product);
+        }
+
+        [Test]
+        public void CorrectSubtotal()
+        {
+            double result = week6.CalculateSubTotal(product);
+
             Assert.That(result, Is.EqualTo(200));
         }
 
         [Test]
         public void CorrectTaxCalculation()
         {
-            //Product product = new Product
-            //{
-            //    BillId = 
-            //}
+            double result = week6.CalculateTax(product, "tax_config.json");
+
+            Assert.That(result, Is.EqualTo(10));
         }
 
+        [Test]
+        public void CorrectFinalAmount()
+        {
+            double result = week6.CalculateFinalAmount(product, "tax_config.json");
+
+            Assert.That(result, Is.EqualTo(210));
+        }
+
+        [Test]
+        public void ZeroQuantity()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    BillId = "BILL1",
+                    Qty = 0,
+                    UnitPrice = 40,
+                    Category = "STATIONERY"
+                }
+            };
+
+            Assert.Throws<InvalidQuantityException>(() => week6.ValidateQuantity(products));
+        }
+
+        [Test]
+        public void NegativeQuantity()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    BillId = "BILL1",
+                    Qty = -5,
+                    UnitPrice = 40,
+                    Category = "STATIONERY"
+                }
+            };
+
+            Assert.Throws<InvalidQuantityException>(() => week6.ValidateQuantity(products));
+        }
+
+        [Test]
+        public void ZeroPrice()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    BillId = "BILL1",
+                    Qty = 5,
+                    UnitPrice = 0,
+                    Category = "STATIONERY"
+                }
+            };
+
+            Assert.Throws<InvalidPriceException>(() => week6.ValidatePrice(products));
+        }
+
+        [Test]
+        public void NegativePrice()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    BillId = "BILL1",
+                    Qty = 5,
+                    UnitPrice = -40,
+                    Category = "STATIONERY"
+                }
+            };
+
+            Assert.Throws<InvalidPriceException>(() => week6.ValidatePrice(products));
+        }
+
+        [Test]
+        public void UnknownCategory()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    BillId = "BILL1",
+                    Qty = 5,
+                    UnitPrice = 40,
+                    Category = "CLOTHES"
+                }
+            };
+
+            Assert.Throws<UnknownCategoryException>(() => week6.ValidateCategory(products, "tax_config.json"));
+        }
     }
 }
