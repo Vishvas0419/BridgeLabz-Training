84f8db4447be17ad9c6f402800dfa3891ba0a389
Vishvas Vaglay <vishvas0419@gmail.com>
2026-09-23 11:41:40 +0530

    Fundoo-Project-Setup

---
+++ BusinessLayer/Interfaces/IUserBL.cs
@@ -0,0 +1,12 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IUserBL
+    {
+       public RegistrationModel RegisterUserBL(RegistrationModel registrationModel);
+    }
+}
---
+++ BusinessLayer/Service/UserBL.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Interfaces;
+using ModelLayer;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class UserBL : IUserBL
+    {
+        public IUserRL _userRL;
+        public UserBL(IUserRL _userRL)
+        {
+            this._userRL = _userRL;
+        }
+        public RegistrationModel RegisterUserBL(RegistrationModel registrationModel)
+        {
+            return _userRL.RegisterUserRL(registrationModel);
+        }
+    }
+}
